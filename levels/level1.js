c07b634315ec05dee9952b0740c96da53dc71728
peet <teilzeitjobbewerber@gmx.de>
2024-05-31 16:20:26 +0200

    Good win screen

--- levels/level1.js
+++ levels/level1.js
@@ -9,17 +9,17 @@ let level1;
 function initLevel(){
   level1 = new Level(
     [
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
-      new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
+      // new Chicken(),
     ],
   
     [
